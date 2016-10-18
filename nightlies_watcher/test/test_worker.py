000bc998de885146ecaead3b33867b343c235aad
Johan Lorenzo <jlorenzo@mozilla.com>
2016-10-18 15:47:32 +0200

    Add unit tests to worker

---
+++ nightlies_watcher/test/test_worker.py
@@ -0,0 +1,166 @@
+import aioamqp
+import asyncio
+import asynctest
+import json
+import pytest
+
+from aioamqp.channel import Channel
+
+from unittest.mock import Mock
+
+from nightlies_watcher import tc_queue, publish
+from nightlies_watcher.exceptions import TaskNotFoundError
+from nightlies_watcher.worker import _dispatch, start_message_queue_worker
+
+
+@pytest.mark.asyncio
+async def test_dispatch(monkeypatch):
+    body = json.dumps({
+        'workerGroup': 'buildbot',
+        'status': {
+            'deadline': '2016-10-15T10:02:46.210Z',
+            'schedulerId': '-',
+            'retriesLeft': 5,
+            'state': 'completed',
+            'expires': '2017-10-15T10:02:46.210Z',
+            'runs': [{
+                'takenUntil': '2016-10-15T09:24:35.326Z',
+                'workerGroup': 'buildbot',
+                'reasonResolved': 'completed',
+                'runId': 0,
+                'reasonCreated': 'scheduled',
+                'scheduled': '2016-10-15T09:02:47.598Z',
+                'state': 'completed',
+                'resolved': '2016-10-15T09:04:37.798Z',
+                'started': '2016-10-15T09:02:48.922Z',
+                'workerId': 'buildbot',
+            }],
+            'taskGroupId': 'QbosbKzTTB2E08IHTAtTfw',
+            'provisionerId': 'null-provisioner',
+            'taskId': 'QbosbKzTTB2E08IHTAtTfw',
+            'workerType': 'buildbot',
+        },
+        'workerId': 'buildbot',
+        'runId': 0,
+        'version': 1,
+    })
+
+    body = body.encode(encoding='utf-8')
+
+    monkeypatch.setattr(tc_queue, 'fetch_task_definition', lambda _: {
+        'provisionerId': 'null-provisioner',
+        'workerType': 'buildbot',
+        'schedulerId': '-',
+        'taskGroupId': 'QbosbKzTTB2E08IHTAtTfw',
+        'dependencies': [],
+        'requires': 'all-completed',
+        'routes': [
+            'index.gecko.v2.mozilla-aurora.nightly.2016.10.15.revision.7bc185ff4e8b66536bf314f9cf8b03f7d7f0b9b8.mobile.android-x86-opt',
+            'index.gecko.v2.mozilla-aurora.nightly.2016.10.15.latest.mobile.android-x86-opt',
+            'index.gecko.v2.mozilla-aurora.nightly.revision.7bc185ff4e8b66536bf314f9cf8b03f7d7f0b9b8.mobile.android-x86-opt',
+            'index.gecko.v2.mozilla-aurora.nightly.latest.mobile.android-x86-opt',
+            'index.buildbot.branches.mozilla-aurora.android-x86',
+            'index.buildbot.revisions.7bc185ff4e8b66536bf314f9cf8b03f7d7f0b9b8.mozilla-aurora.android-x86'
+        ],
+        'priority': 'normal',
+        'retries': 5,
+        'created': '2016-10-15T09:02:46.210Z',
+        'deadline': '2016-10-15T10:02:46.210Z',
+        'expires': '2017-10-15T10:02:46.210Z',
+        'scopes': [],
+        'payload': {},
+        'metadata': {
+            'owner': 'mshal@mozilla.com',
+            'source': 'http://hg.mozilla.org/build/mozharness/',
+            'name': 'Buildbot/mozharness S3 uploader',
+            'description': 'Upload outputs of buildbot/mozharness builds to S3'
+        },
+        'tags': {},
+        'extra': {
+            'index': {
+                'rank': 1476471372
+            }
+        }
+    })
+
+    monkeypatch.setattr(publish, 'publish', lambda _, __, ___: None)
+
+    channel = asynctest.mock.Mock(Channel)
+    envelope = Mock()
+    envelope.delivery_tag = asynctest.MagicMock()
+
+    await _dispatch(channel, body, envelope, None)
+    channel.basic_client_ack.assert_called_once_with(delivery_tag=envelope.delivery_tag)
+
+    def raise_task_not_found(_, __, ___):
+        raise TaskNotFoundError('', '', '')
+
+    monkeypatch.setattr(publish, 'publish', raise_task_not_found)
+    # TaskNotFoundError should be processed within _dispatch
+    await _dispatch(channel, body, envelope, None)
+
+    def raise_other_exception(_, __, ___):
+        raise Exception()
+
+    monkeypatch.setattr(publish, 'publish', raise_other_exception)
+    # Other exceptions should be caught by the general trap
+    await _dispatch(channel, body, envelope, None)
+
+
+@pytest.mark.asyncio
+async def test_start_message_queue_worker(monkeypatch):
+    config = {
+        'pulse': {
+            'host': 'pulse.m.o',
+            'port': '5671',
+            'user': 'a-user',
+            'password': 'a-password',
+            'queue': 'a-queue',
+            'exchanges': [{
+              'path': 'exchange/taskcluster-queue/v1/task-completed',
+              'routing_keys': ['route.index.gecko.v2.mozilla-aurora.nightly.latest.mobile.#']
+            }]
+        }
+    }
+
+    channel_mock = asynctest.CoroutineMock()
+    channel_mock.queue_declare = asynctest.CoroutineMock()
+    channel_mock.queue_bind = asynctest.CoroutineMock()
+
+    @asyncio.coroutine
+    def mock_channel():
+        return channel_mock
+
+    protocol_mock = asynctest.Mock(asyncio.Protocol)
+    protocol_mock.channel = mock_channel
+
+    @asyncio.coroutine
+    def mock_connection(host, login, password, ssl, port):
+        assert host == 'pulse.m.o'
+        assert port == '5671'
+        assert login == 'a-user'
+        assert password == 'a-password'
+        assert ssl is True
+
+        return (None, protocol_mock)
+
+    monkeypatch.setattr(aioamqp, 'connect', mock_connection)
+
+    await start_message_queue_worker(config)
+
+    expected_queue_name = 'queue/a-user/a-queue'
+    channel_mock.queue_declare.assert_called_once_with(queue_name=expected_queue_name, durable=True)
+    channel_mock.queue_bind.assert_called_once_with(
+        exchange_name='exchange/taskcluster-queue/v1/task-completed',
+        queue_name=expected_queue_name,
+        routing_key='route.index.gecko.v2.mozilla-aurora.nightly.latest.mobile.#'
+    )
+
+    # start_message_queue_worker() should early return
+    def raise_connection_error(host, login, password, ssl, port):
+        raise aioamqp.AmqpClosedConnection()
+
+    channel_mock.queue_declare.reset_mock()
+    monkeypatch.setattr(aioamqp, 'connect', raise_connection_error)
+    await start_message_queue_worker(config)
+    channel_mock.queue_declare.assert_not_called()
--- nightlies_watcher/worker.py
+++ nightlies_watcher/worker.py
@@ -2,22 +2,19 @@ import aioamqp
 import logging
 import json
 
-from nightlies_watcher import tc_queue
+from nightlies_watcher import tc_queue, publish
 from nightlies_watcher.exceptions import TaskNotFoundError
-from nightlies_watcher.publish import publish
 from nightlies_watcher.config import get_config
 
 
 log = logging.getLogger(__name__)
 
-async def worker():
-    config = get_config()
-    log.warn(config)
 
+async def start_message_queue_worker(config):
     pulse_config = config['pulse']
 
     try:
-        transport, protocol = await aioamqp.connect(
+        _, protocol = await aioamqp.connect(
             host=pulse_config['host'],
             login=pulse_config['user'],
             password=pulse_config['password'],
@@ -29,10 +26,11 @@ async def worker():
         return
 
     channel = await protocol.channel()
-    await channel.basic_qos(prefetch_count=1, prefetch_size=0,
-                            connection_global=False)
+    await channel.basic_qos(prefetch_count=1, prefetch_size=0, connection_global=False)
+
     queue_name = 'queue/{}/{}'.format(pulse_config['user'], pulse_config['queue'])
     log.info('Using queue: %s', queue_name)
+
     await channel.queue_declare(queue_name=queue_name, durable=True)
     for exchange in pulse_config['exchanges']:
         for key in exchange['routing_keys']:
@@ -45,11 +43,9 @@ async def worker():
     log.info('Worker has completed running.')
 
 
-async def _dispatch(channel, body, envelope, properties):
+async def _dispatch(channel, body, envelope, _):
     body = json.loads(body.decode('utf-8'))
-    log.debug('Got a new message from the queue. Channel: {}. Body: {}. Envelope: {}. Properties: {}'.format(
-        channel, body, envelope, properties
-    ))
+    log.warn('Got a new message from the queue. Body: {}'.format(body))
 
     task_id = body['status']['taskId']
     config = get_config()
@@ -62,7 +58,7 @@ async def _dispatch(channel, body, envelope, properties):
         log.info('Processing revision "{}" from repository "{}" (triggered by completed task "{}")'.format(
             revision, repository, task_id
         ))
-        publish(config, repository, revision)
+        publish.publish(config, repository, revision)
 
     except TaskNotFoundError as e:
         log.info('Revision "{}" does not have architecture "{}" completed yet'.format(
@@ -73,26 +69,5 @@ async def _dispatch(channel, body, envelope, properties):
         log.exception('Exception {} caught by generic exception trap'.format(e))
 
     finally:
-        log.info('Processed task "{}". Definition: {}'.format(task_id, task_definition))
-
-        # TODO Remove that queue cleaner
-        if does_route_contain_firefox(task_definition):
-            log.info('Automatically acknowledging consumption of %r', task_id)
-            return await channel.basic_client_ack(delivery_tag=envelope.delivery_tag)
-
-        response = input('Should we keep it in the queue?')
-        if response == 'n':
-            log.info('Acknowledging consumption of %r', task_id)
-            return await channel.basic_client_ack(delivery_tag=envelope.delivery_tag)
-        else:
-            log.info('Keeping the task')
-
-
-def does_route_contain_firefox(task_definition):
-    matching_routes = [
-        route
-        for route in task_definition['routes']
-        if 'firefox' in route
-    ]
-
-    return len(matching_routes) > 0
+        log.info('Marking message as read. Processed task "{}". Definition: {}'.format(task_id, task_definition))
+        await channel.basic_client_ack(delivery_tag=envelope.delivery_tag)
