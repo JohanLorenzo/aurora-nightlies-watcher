ec740511168ecd47f34701d7661963f8a627bdb2
Johan Lorenzo <jlorenzo@mozilla.com>
2016-10-18 19:03:18 +0200

    Make config failing over default one

--- nightlies_watcher/config.py
+++ nightlies_watcher/config.py
@@ -3,23 +3,37 @@ import logging
 import os
 from frozendict import frozendict
 
-from nightlies_watcher.directories import PROJECT_DIRECTORY
+from nightlies_watcher.directories import PROJECT_DIRECTORY, DATA_DIRECTORY
 
 logger = logging.getLogger(__name__)
 
-CONFIGS = {}
+_configs = {}
+
+DEFAULT_LOCATION = os.path.join(PROJECT_DIRECTORY, 'config.json')
+DEFAULT_CONFIG_LOCATION = os.path.join(DATA_DIRECTORY, 'config.default.json')
 
 
-def get_config(path=None):
-    config_path = path or os.path.join(PROJECT_DIRECTORY, 'config.json')
+def get_config(config_path=None):
+    config_path = config_path or DEFAULT_LOCATION
 
     try:
-        return CONFIGS[config_path]
+        return _configs[config_path]
     except KeyError:
-        logger.debug('Loading config file at "{}"'.format(config_path))
-
-        with open(config_path) as f:
-            config = frozendict(json.load(f))
-
-        CONFIGS[config_path] = config
+        config = _get_config_from_file_or_default_one(config_path)
+        _configs[config_path] = config
         return config
+
+
+def _get_config_from_file_or_default_one(config_path):
+    logger.debug('Loading config file at "{}"'.format(config_path))
+
+    try:
+        return _load_config(config_path)
+    except FileNotFoundError:
+        logger.warn('"{}" not found. Loading default config at: {}'.format(config_path, DEFAULT_CONFIG_LOCATION))
+        return _load_config(DEFAULT_CONFIG_LOCATION)
+
+
+def _load_config(config_path):
+    with open(config_path) as f:
+        return frozendict(json.load(f))
