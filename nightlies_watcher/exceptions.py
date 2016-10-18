f721126c6ee7ec224f422c1d548b25ac38ae0699
Johan Lorenzo <jlorenzo@mozilla.com>
2016-10-18 12:15:53 +0200

    Explicitly log when tasks are missing

--- nightlies_watcher/exceptions.py
+++ nightlies_watcher/exceptions.py
@@ -16,3 +16,11 @@ class UnmatchedRouteError(Exception):
 class NotOnlyOneApkError(Exception):
     def __init__(self, matched_artifacts):
         super().__init__('Not only one artifact matches the APK regex. Matched artifacts: {}'.format(matched_artifacts))
+
+
+class TaskNotFoundError(Exception):
+    def __init__(self, repository, revision, missing_android_architecture):
+        self.missing_android_architecture = missing_android_architecture
+        super().__init__('Task for architecture {} and revision {} in repository {} does not exist'.format(
+            missing_android_architecture, revision, repository
+        ))
