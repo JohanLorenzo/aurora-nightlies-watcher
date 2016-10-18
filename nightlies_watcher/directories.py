c932ff1c05a5f31f447fad207dc1cfdef84861c7
Johan Lorenzo <jlorenzo@mozilla.com>
2016-10-18 19:03:18 +0200

    Make config failing over default one

--- nightlies_watcher/directories.py
+++ nightlies_watcher/directories.py
@@ -3,3 +3,4 @@ import os
 
 PACKAGE_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 PROJECT_DIRECTORY = os.path.join(PACKAGE_DIRECTORY, '..')
+DATA_DIRECTORY = os.path.join(PACKAGE_DIRECTORY, 'data')
--- nightlies_watcher/test/test_config.py
+++ nightlies_watcher/test/test_config.py
@@ -5,6 +5,7 @@ import tempfile
 from frozendict import frozendict
 
 from nightlies_watcher.config import get_config
+from nightlies_watcher.directories import DATA_DIRECTORY
 
 
 def test_get_config():
@@ -16,10 +17,17 @@ def test_get_config():
         with open(config_path, 'w') as f:
             json.dump(original_config, f)
 
-        config = get_config(path=config_path)
+        config = get_config(config_path=config_path)
         assert config == original_config
         assert isinstance(config, frozendict)
 
     # Second call doesn't reload file
-    second_config = get_config(path=config_path)
+    second_config = get_config(config_path=config_path)
     assert second_config is config
+
+    # Non-exising config loads default one
+    default_config = get_config(config_path='/non/existing/path')
+    with open(os.path.join(DATA_DIRECTORY, 'config.default.json')) as f:
+        expected_default_config = json.load(f)
+
+    assert default_config == expected_default_config
