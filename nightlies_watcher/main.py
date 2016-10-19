2ea2f8b20219c8d34bf1b8e0c117c6b3e7c4c698
Johan Lorenzo <jlorenzo@mozilla.com>
2016-10-19 14:18:02 +0200

    Add unit test to main

--- nightlies_watcher/main.py
+++ nightlies_watcher/main.py
@@ -1,21 +1,14 @@
 import asyncio
 import logging
-import os
 import taskcluster
 
 from nightlies_watcher.config import get_config
 from nightlies_watcher.worker import start_message_queue_worker
 
-event_loop = asyncio.get_event_loop()
-
-
 logger = logging.getLogger(__name__)
 
-CURRENT_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
-PROJECT_DIRECTORY = os.path.join(CURRENT_DIRECTORY, '..')
 
-
-def main(name):
+def main(name=None):
     if name not in ('__main__', None):
         return
 
@@ -27,6 +20,8 @@ def main(name):
 
     taskcluster.config['credentials']['clientId'] = config['credentials']['client_id']
     taskcluster.config['credentials']['accessToken'] = config['credentials']['access_token']
+
+    event_loop = asyncio.get_event_loop()
 
     try:
         event_loop.run_until_complete(start_message_queue_worker(config))
