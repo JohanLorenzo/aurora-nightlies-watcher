6292f8cf8b65c762c2ebf1653107803c02dc8e9c
Johan Lorenzo <jlorenzo@mozilla.com>
2016-10-20 15:09:13 +0200

    Implemented ability to have configuration in either ENV var or config.json

--- fennec_aurora_task_creator/config.py
+++ fennec_aurora_task_creator/config.py
@@ -1,9 +1,13 @@
+import copy
 import json
 import logging
 import os
+
+from collections import defaultdict
 from frozendict import frozendict
 
 from fennec_aurora_task_creator.directories import PROJECT_DIRECTORY, DATA_DIRECTORY
+from fennec_aurora_task_creator.exceptions import MissingConfigurationError
 
 logger = logging.getLogger(__name__)
 
@@ -11,6 +15,34 @@ _configs = {}
 
 DEFAULT_LOCATION = os.path.join(PROJECT_DIRECTORY, 'config.json')
 DEFAULT_CONFIG_LOCATION = os.path.join(DATA_DIRECTORY, 'config.default.json')
+
+KEYS_AND_DEFAULT_VALUES = (
+    {'path': 'credentials/client_id', 'environment_key': 'TASKCLUSTER_CLIENT_ID'},
+    {'path': 'credentials/access_token', 'environment_key': 'TASKCLUSTER_ACCESS_TOKEN'},
+
+    {'path': 'task/name', 'environment_key': 'TASK_NAME', 'default_value': 'Google Play Publisher'},
+    {'path': 'task/description', 'environment_key': 'TASK_DESCRIPTION', 'default_value': 'Publishes Aurora builds to Google Play Store'},
+    {'path': 'task/owner', 'environment_key': 'TASK_OWNER_EMAIL'},
+    {'path': 'task/provisioner_id', 'environment_key': 'TASK_PROVISIONER_ID'},
+    {'path': 'task/worker_type', 'environment_key': 'TASK_WORKER_TYPE'},
+    {'path': 'task/scopes', 'environment_key': 'TASK_SCOPES'},
+    {'path': 'task/google_play_track', 'environment_key': 'TASK_GOOGLE_PLAY_TRACK', 'default_value': 'alpha'},
+
+    {'path': 'task/treeherder/platform', 'environment_key': 'TREEHERDER_PLATFORM', 'default_value': 'Android'},
+    {'path': 'task/treeherder/group_name', 'environment_key': 'TREEHERDER_GROUP_NAME', 'default_value': 'Publisher'},
+    {'path': 'task/treeherder/group_symbol', 'environment_key': 'TREEHERDER_GROUP_SYMBOL', 'default_value': 'pub'},
+    {'path': 'task/treeherder/symbol', 'environment_key': 'TREEHERDER_JOB_SYMBOL', 'default_value': 'gp'},
+    {'path': 'task/treeherder/reason', 'environment_key': 'TREEHERDER_JOB_REASON'},
+    {'path': 'task/treeherder/tier', 'environment_key': 'TREEHERDER_TIER', 'default_value': 3},
+    {'path': 'task/treeherder/is_opt', 'environment_key': 'TREEHERDER_IS_OPT', 'default_value': True},
+
+    {'path': 'pulse/host', 'environment_key': 'PULSE_HOST', 'default_value': 'pulse.mozilla.org'},
+    {'path': 'pulse/port', 'environment_key': 'PULSE_PORT', 'default_value': 5671},
+    {'path': 'pulse/user', 'environment_key': 'PULSE_USER'},
+    {'path': 'pulse/password', 'environment_key': 'PULSE_PASSWORD'},
+    {'path': 'pulse/queue', 'environment_key': 'PULSE_QUEUE_NAME'},
+    {'path': 'verbose', 'environment_key': 'VERBOSE_MODE', 'default_value': False},
+)
 
 
 def get_config(config_path=None):
@@ -37,3 +69,59 @@ def _get_config_from_file_or_default_one(config_path):
 def _load_config(config_path):
     with open(config_path) as f:
         return frozendict(json.load(f))
+
+
+def _generate_final_config_object(config_from_json_file):
+    target_config_dict = _recursive_defaultdict()
+
+    for key_and_default_value in KEYS_AND_DEFAULT_VALUES:
+        target_config_dict = _add_configuration(
+            target_config_dict, config_from_json_file, path_string=key_and_default_value['path'],
+            environment_key=key_and_default_value['environment_key'],
+            default_value=key_and_default_value.get('default_value', None)
+        )
+
+    return target_config_dict
+
+
+def _recursive_defaultdict():
+    return defaultdict(_recursive_defaultdict)
+
+
+def _add_configuration(target_config_dict, config_from_json_file, path_string, environment_key, default_value=None):
+    target_config_dict = copy.copy(target_config_dict)
+    path_list = path_string.split('/')
+
+    _set_dict_path(
+        target_config_dict,
+        path_list,
+        _get_environment_or_config_or_default_value(
+            config_from_json_file, path_list, environment_key, default_value=default_value
+        )
+    )
+    return target_config_dict
+
+
+def _get_environment_or_config_or_default_value(config_from_json_file, path_list, environment_key, default_value=None):
+    try:
+        value = os.environ[environment_key]
+    except KeyError:
+        try:
+            value = _get_dict_path(config_from_json_file, path_list)
+        except KeyError:
+            value = default_value
+
+    if value is None:
+        raise MissingConfigurationError(environment_key, '/'.join(path_list))
+    return value
+
+
+def _set_dict_path(recursive_dictionary, path_list, value):
+    if len(path_list) == 1:
+        recursive_dictionary[path_list[0]] = value
+    else:
+        _set_dict_path(recursive_dictionary[path_list[0]], path_list[1:], value)
+
+
+def _get_dict_path(dictionary, path_list):
+    return dictionary[path_list[0]] if len(path_list) == 1 else _get_dict_path(dictionary[path_list[0]], path_list[1:])
--- fennec_aurora_task_creator/test/test_config.py
+++ fennec_aurora_task_creator/test/test_config.py
@@ -1,11 +1,15 @@
 import json
 import os
+import pytest
 import tempfile
 
+from copy import copy
 from frozendict import frozendict
 
-from fennec_aurora_task_creator.config import get_config
+from fennec_aurora_task_creator.config import get_config, _generate_final_config_object, _recursive_defaultdict, _add_configuration, \
+    _get_environment_or_config_or_default_value
 from fennec_aurora_task_creator.directories import DATA_DIRECTORY
+from fennec_aurora_task_creator.exceptions import MissingConfigurationError
 
 
 def test_get_config():
@@ -31,3 +35,101 @@ def test_get_config():
         expected_default_config = json.load(f)
 
     assert default_config == expected_default_config
+
+
+MINIMAL_CONFIG = {
+    'credentials': {
+        'client_id': 'dummy-client-id',
+        'access_token': 'dummy-token'
+    },
+    'task': {
+        'owner': 'r@m.c',
+        'provisioner_id': 'dummy-provisioner',
+        'worker_type': 'dummy-worker',
+        'scopes': [],
+        'treeherder': {
+            'symbol': 'dum',
+            'reason': 'Dummy reason',
+        }
+    },
+    'pulse': {
+        'user': 'dummy-user',
+        'password': 'dummy-password',
+        'queue': 'dummy-queue'
+    }
+}
+
+
+def test_generate_final_config_object_allows_full_config_to_be_a_dict_alone():
+    final_config = _generate_final_config_object(MINIMAL_CONFIG)
+    assert final_config['credentials']['access_token'] == 'dummy-token'
+
+
+def test_generate_final_config_object_fulfills_default_values():
+    final_config = _generate_final_config_object(MINIMAL_CONFIG)
+
+    assert final_config['task']['name'] == 'Google Play Publisher'
+    assert final_config['task']['description'] == 'Publishes Aurora builds to Google Play Store'
+    assert final_config['task']['google_play_track'] == 'alpha'
+
+    assert final_config['task']['treeherder']['platform'] == 'Android'
+    assert final_config['task']['treeherder']['group_name'] == 'Publisher'
+    assert final_config['task']['treeherder']['group_symbol'] == 'pub'
+    assert final_config['task']['treeherder']['tier'] == 3
+    assert final_config['task']['treeherder']['is_opt']
+
+    assert final_config['pulse']['host'] == 'pulse.mozilla.org'
+    assert final_config['pulse']['port'] == 5671
+
+    assert not(final_config['verbose'])
+
+
+def test_generate_final_config_object_allows_environment_variables():
+    os.environ['TASKCLUSTER_ACCESS_TOKEN'] = 'dummy-env-token'
+
+    final_config = _generate_final_config_object(MINIMAL_CONFIG)
+    assert final_config['credentials']['access_token'] == 'dummy-env-token'
+
+    del os.environ['TASKCLUSTER_ACCESS_TOKEN']
+
+
+def test_generate_final_config_object_reports_missing_mandatory_configuration():
+    initial_config = copy(MINIMAL_CONFIG)
+    del initial_config['credentials']['access_token']
+
+    with pytest.raises(MissingConfigurationError):
+        _generate_final_config_object(initial_config)
+
+
+def test_recursive_default_dict():
+    recursive_dict = _recursive_defaultdict()
+    recursive_dict['does']['not']['throw']['an']['error'] = 'random value'
+
+
+def test_add_configuration_does_not_modify_input_dict():
+    target_dict = _recursive_defaultdict()
+    target_dict2 = _add_configuration(target_dict, {}, 'many/keys', 'MANY_KEYS', default_value='value')
+    assert target_dict2['many']['keys'] == 'value'
+    assert target_dict['many']['keys'] == {}
+
+
+def test_get_environment_or_config_or_default_value():
+    path_list = ('many', 'keys')
+    env_key = 'MANY_KEYS'
+
+    config_json = {}
+    with pytest.raises(MissingConfigurationError):
+        _get_environment_or_config_or_default_value(config_json, path_list, env_key)
+
+    default_value = 'defaut_value'
+    assert _get_environment_or_config_or_default_value(config_json, path_list, env_key, default_value) == \
+        'defaut_value'
+
+    config_json['many'] = {'keys': 'json_value'}
+    assert _get_environment_or_config_or_default_value(config_json, path_list, env_key, default_value) == \
+        'json_value'
+
+    os.environ['MANY_KEYS'] = 'environment_value'
+    assert _get_environment_or_config_or_default_value(config_json, path_list, env_key, default_value) == \
+        'environment_value'
+    del os.environ['MANY_KEYS']
