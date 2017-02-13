ada2e4d7f17b0609552971ea2b4b1ccb32e145de
Johan Lorenzo <jlorenzo@mozilla.com>
2017-02-13 12:14:34 +0100

    Follow up tc-migration: Update remaining tests

--- fennec_aurora_task_creator/test/test_tc_index.py
+++ fennec_aurora_task_creator/test/test_tc_index.py
@@ -8,41 +8,33 @@ from fennec_aurora_task_creator.tc_index import get_task_id, _craft_full_namespa
 def test_craft_full_namespace():
     # Supports en-US only builds
     assert _craft_full_namespace(
-        config={'taskcluster_index_pattern': 'gecko.v2.{repository}.nightly.revision.{revision}.mobile.{architecture}'},
+        config={'taskcluster_index_pattern': 'gecko.v2.{repository}.signed-nightly.nightly.revision.{revision}.mobile.{architecture}'},
         repository='mozilla-aurora',
-        revision='7bc185ff4e8b66536bf314f9cf8b03f7d7f0b9b8',
+        revision='6b063631a7d3ffd5dc2b621852e4d8ac8758ef99',
         android_architecture='android-api-15-opt'
-    ) == 'gecko.v2.mozilla-aurora.nightly.revision.7bc185ff4e8b66536bf314f9cf8b03f7d7f0b9b8.mobile.android-api-15-opt'
-
-    # Supports multi-locale builds
-    assert _craft_full_namespace(
-        config={'taskcluster_index_pattern': 'gecko.v2.{repository}.revision.{revision}.mobile-l10n.{architecture}.multi'},
-        repository='mozilla-aurora',
-        revision='c683b0d41a52ffab980165b79b9c82590001354b',
-        android_architecture='android-api-15-opt'
-    ) == 'gecko.v2.mozilla-aurora.revision.c683b0d41a52ffab980165b79b9c82590001354b.mobile-l10n.android-api-15-opt.multi'
+    ) == 'gecko.v2.mozilla-aurora.signed-nightly.nightly.revision.6b063631a7d3ffd5dc2b621852e4d8ac8758ef99.mobile.android-api-15-opt'
 
 
 def test_get_task_id(monkeypatch):
     config = {
-        'taskcluster_index_pattern': 'gecko.v2.{repository}.revision.{revision}.mobile-l10n.{architecture}.multi',
+        'taskcluster_index_pattern': 'gecko.v2.{repository}.signed-nightly.nightly.revision.{revision}.mobile.{architecture}',
     }
 
     monkeypatch.setattr(
         _index, 'findTask',
-        lambda namespace: {'taskId': 'cMu_aqFiTD-UJ5l2K1TaUw'} if namespace ==
-        'gecko.v2.mozilla-aurora.revision.c683b0d41a52ffab980165b79b9c82590001354b.mobile-l10n.android-x86-opt.multi'
+        lambda namespace: {'taskId': 'Klg1GnM4TAqeBqgz-FOdtw'} if namespace ==
+        'gecko.v2.mozilla-aurora.signed-nightly.nightly.revision.6b063631a7d3ffd5dc2b621852e4d8ac8758ef99.mobile.android-x86-opt'
         else None
     )
-    assert get_task_id(config, 'mozilla-aurora', 'c683b0d41a52ffab980165b79b9c82590001354b', 'android-x86-opt') == \
-        'cMu_aqFiTD-UJ5l2K1TaUw'
+    assert get_task_id(config, 'mozilla-aurora', '6b063631a7d3ffd5dc2b621852e4d8ac8758ef99', 'android-x86-opt') == \
+        'Klg1GnM4TAqeBqgz-FOdtw'
 
     def not_found(_):
         raise taskcluster.exceptions.TaskclusterRestFailure(msg='not found', superExc=None, status_code=404)
 
     monkeypatch.setattr(_index, 'findTask', not_found)
     with pytest.raises(TaskNotFoundError):
-        get_task_id(config, 'mozilla-aurora', '7bc185ff4e8b66536bf314f9cf8b03f7d7f0b9b', 'android-x86-opt')
+        get_task_id(config, 'mozilla-aurora', '6b063631a7d3ffd5dc2b621852e4d8ac8758ef99', 'android-x86-opt')
 
     # Other errors than 404 should not be filtered out
     def other_error(_):
@@ -50,4 +42,4 @@ def test_get_task_id(monkeypatch):
 
     monkeypatch.setattr(_index, 'findTask', other_error)
     with pytest.raises(taskcluster.exceptions.TaskclusterRestFailure):
-        get_task_id(config, 'mozilla-aurora', '7bc185ff4e8b66536bf314f9cf8b03f7d7f0b9b', 'android-x86-opt')
+        get_task_id(config, 'mozilla-aurora', '6b063631a7d3ffd5dc2b621852e4d8ac8758ef99', 'android-x86-opt')
