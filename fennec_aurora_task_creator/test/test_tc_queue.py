3b99f9012497d7a233a22eb4fe74ac511654e439
Johan Lorenzo <jlorenzo@mozilla.com>
2017-02-13 12:14:34 +0100

    Follow up tc-migration: Update remaining tests

--- fennec_aurora_task_creator/test/test_tc_queue.py
+++ fennec_aurora_task_creator/test/test_tc_queue.py
@@ -4,15 +4,20 @@ from fennec_aurora_task_creator.tc_queue import fetch_task_definition, pluck_rep
     _queue, fetch_artifacts_list, create_task, _get_regex_pattern_from_string_pattern
 from fennec_aurora_task_creator.exceptions import UnmatchedRouteError
 
-ROUTE_PATTERN = 'gecko.v2.{repository}.revision.{revision}.mobile-l10n.{architecture}.multi'
+ROUTE_PATTERN = 'gecko.v2.{repository}.signed-nightly.nightly.revision.{revision}.mobile.{architecture}'
 
 TASK_DEFINITION = {
     'routes': [
-        'index.gecko.v2.mozilla-aurora.revision.d9cfe58247e85c05ad98a4e60045bbdd62e0ec2b.mobile-l10n.android-api-15-opt.multi',
-        'index.gecko.v2.mozilla-aurora.pushdate.2016.11.08.20161108081244.mobile-l10n.android-api-15-opt.multi',
-        'index.gecko.v2.mozilla-aurora.latest.mobile-l10n.android-api-15-opt.multi',
-        'index.buildbot.branches.mozilla-aurora.android-api-15',
-        'index.buildbot.revisions.d9cfe58247e85c05ad98a4e60045bbdd62e0ec2b.mozilla-aurora.android-api-15',
+        'index.gecko.v2.mozilla-aurora.signed-nightly.nightly.latest.mobile.android-x86-opt',
+        'index.gecko.v2.mozilla-aurora.signed-nightly.nightly.2017.02.10.revision.6b063631a7d3ffd5dc2b621852e4d8ac8758ef99.mobile.android-x86-opt',
+        'index.gecko.v2.mozilla-aurora.signed-nightly.nightly.2017.02.10.latest.mobile.android-x86-opt',
+        'index.gecko.v2.mozilla-aurora.signed-nightly.nightly.revision.6b063631a7d3ffd5dc2b621852e4d8ac8758ef99.mobile.android-x86-opt',
+        'index.gecko.v2.mozilla-aurora.signed-nightly.revision.6b063631a7d3ffd5dc2b621852e4d8ac8758ef99.mobile-l10n.android-x86-opt.en-US',
+        'index.gecko.v2.mozilla-aurora.signed-nightly.pushdate.2017.02.10.20170210084116.mobile-l10n.android-x86-opt.en-US',
+        'index.gecko.v2.mozilla-aurora.signed-nightly.latest.mobile-l10n.android-x86-opt.en-US',
+        'index.project.releng.funsize.level-3.mozilla-aurora',
+        'tc-treeherder.v2.mozilla-aurora.6b063631a7d3ffd5dc2b621852e4d8ac8758ef99.0',
+        'tc-treeherder-stage.v2.mozilla-aurora.6b063631a7d3ffd5dc2b621852e4d8ac8758ef99.0',
     ]
 }
 
@@ -21,14 +26,14 @@ def test_pluck_repository():
     assert pluck_repository(ROUTE_PATTERN, TASK_DEFINITION) == 'mozilla-aurora'
 
     with pytest.raises(UnmatchedRouteError):
-        # Not mobile-l10n
+        # Not signed signed-nightly
         pluck_repository(ROUTE_PATTERN, {
-            'routes': ['index.gecko.v2.mozilla-aurora.revision.7bc185ff4e8b66536bf314f9cf8b03f7d7f0b9b8.mobile.android-api-15']
+            'routes': ['index.gecko.v2.mozilla-aurora.revision.7bc185ff4e8b66536bf314f9cf8b03f7d7f0b9b8.mobile-l10n.android-api-15']
         })
 
 
 def test_pluck_revision():
-    assert pluck_revision(ROUTE_PATTERN, TASK_DEFINITION) == 'd9cfe58247e85c05ad98a4e60045bbdd62e0ec2b'
+    assert pluck_revision(ROUTE_PATTERN, TASK_DEFINITION) == '6b063631a7d3ffd5dc2b621852e4d8ac8758ef99'
 
     with pytest.raises(UnmatchedRouteError):
         # No revision
@@ -39,50 +44,59 @@ def test_pluck_revision():
 
 def test_get_regex_pattern_from_string_pattern():
     assert _get_regex_pattern_from_string_pattern(ROUTE_PATTERN) == \
-        r'index\.gecko\.v2\.([^.]+)\.revision\.([^.]+)\.mobile-l10n\.([^.]+)\.multi'
+        r'index\.gecko\.v2\.([^.]+)\.signed-nightly\.nightly\.revision\.([^.]+)\.mobile\.([^.]+)'
 
 
 def test_fetch_task_definition(monkeypatch):
     monkeypatch.setattr(_queue, 'task', lambda _: TASK_DEFINITION)
-    assert fetch_task_definition('VRzn3vi6RvSNaKTaT5u83A') == TASK_DEFINITION
+    assert fetch_task_definition('Klg1GnM4TAqeBqgz-FOdtw') == TASK_DEFINITION
 
 
 def test_fetch_artifacts_list(monkeypatch):
     artifacts = [{
-        'contentType': 'application/json',
         'storageType': 's3',
-        'name': 'public/build/buildbot_properties.json',
-        'expires': '2017-10-14T09:07:47.161Z'
+        'name': 'public/build/en-US/target.apk',
+        'expires': '2017-02-24T09:35:26.674Z',
+        'contentType': 'application/binary',
     }, {
-        'contentType': 'application/octet-stream',
         'storageType': 's3',
-        'name': 'public/build/fennec-51.0a2.en-US.android-arm.apk',
-        'expires': '2017-10-14T08:57:10.154Z'
+        'name': 'public/build/target.apk',
+        'expires': '2017-02-24T09:35:26.683Z',
+        'contentType': 'application/binary',
     }]
 
     monkeypatch.setattr(_queue, 'listLatestArtifacts', lambda _: {
         'artifacts': artifacts,
     })
-    assert fetch_artifacts_list('VRzn3vi6RvSNaKTaT5u83A') == artifacts
+    assert fetch_artifacts_list('Klg1GnM4TAqeBqgz-FOdtw') == artifacts
 
 
 def test_create_task(monkeypatch):
     created_task_data = {
         'status': {
-            'provisionerId': 'dummy-provisioner',
-            'taskGroupId': 'LBrUAO8NRDmFbv5JMrm3vQ',
-            'state': 'pending',
-            'workerType': 'dummy-worker',
-            'retriesLeft': 0,
-            'schedulerId': '-',
-            'taskId': 'LBrUAO8NRDmFbv5JMrm3vQ',
-            'deadline': '2016-10-17T17:43:47.961Z',
-            'expires': '2017-10-17T17:43:47.961Z',
-            'runs': [
-                {'scheduled': '2016-10-17T16:43:49.669Z', 'state': 'pending', 'reasonCreated': 'scheduled', 'runId': 0}
-            ],
-        },
+            'taskId': 'Klg1GnM4TAqeBqgz-FOdtw',
+            'provisionerId': 'scriptworker-prov-v1',
+            'workerType': 'signing-linux-v1',
+            'schedulerId': 'gecko-level-3',
+            'taskGroupId': 'GZItMiWuRbaAy5W4WgpwjQ',
+            'deadline': '2017-02-11T08:41:28.419Z',
+            'expires': '2018-02-10T08:41:28.419Z',
+            'retriesLeft': 5,
+            'state': 'completed',
+            'runs': [{
+                'runId': 0,
+                'state': 'completed',
+                'reasonCreated': 'scheduled',
+                'reasonResolved': 'completed',
+                'workerGroup': 'signing-linux-v1',
+                'workerId': 'signing-linux-4',
+                'takenUntil': '2017-02-10T09:54:56.358Z',
+                'scheduled': '2017-02-10T09:34:52.833Z',
+                'started': '2017-02-10T09:34:56.446Z',
+                'resolved': '2017-02-10T09:35:29.971Z',
+            }]
+        }
     }
 
     monkeypatch.setattr(_queue, 'createTask', lambda payload, taskId: created_task_data)
-    assert create_task(payload={}, task_id='LBrUAO8NRDmFbv5JMrm3vQ') == created_task_data
+    assert create_task(payload={}, task_id='Klg1GnM4TAqeBqgz-FOdtw') == created_task_data
